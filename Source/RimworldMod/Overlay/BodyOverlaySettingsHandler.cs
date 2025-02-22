61a26b3bbade2104e5a5827d3a4a902a9d7a9911
Thamuzz1331 <alexanderelliott121@gmail.com>
2025-02-22 08:46:13 -0500

    Update load folders.

--- Source/RimworldMod/Overlay/BodyOverlaySettingsHandler.cs
+++ Source/RimworldMod/Overlay/BodyOverlaySettingsHandler.cs
@@ -8,22 +8,18 @@ using Verse;
 
 namespace RimWorld
 {
-	// Token: 0x02000002 RID: 2
 	internal class BodyOverlaySettingsHandler : Mod
 	{
-		// Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
 		public BodyOverlaySettingsHandler(ModContentPack content) : base(content)
 		{
 			BodyOverlaySettingsHandler.Settings = base.GetSettings<BodyOverlaySettings>();
 		}
 
-		// Token: 0x06000002 RID: 2 RVA: 0x00002068 File Offset: 0x00000268
 		public override string SettingsCategory()
 		{
 			return "Living Building";
 		}
 
-		// Token: 0x06000003 RID: 3 RVA: 0x00002080 File Offset: 0x00000280
 		public override void DoSettingsWindowContents(Rect rect)
 		{
 			Listing_Standard listing_Standard = new Listing_Standard();
@@ -41,14 +37,12 @@ namespace RimWorld
 			base.DoSettingsWindowContents(rect);
 		}
 
-		// Token: 0x06000004 RID: 4 RVA: 0x000022BC File Offset: 0x000004BC
 		private void ResetAll()
 		{
 			BodyOverlaySettingsHandler.Settings.opacity = 0.5f;
 			BodyOverlaySettingsHandler.Settings.baseUpdateTicks = 120;
 		}
 
-		// Token: 0x04000001 RID: 1
 		public static BodyOverlaySettings Settings;
 	}
 }
